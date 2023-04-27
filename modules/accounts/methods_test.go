5b5128e2338ff19f9924a866f19828d03013b456
Muaz Wazir <muazwazir@gmail.com>
2023-04-27 15:19:58 +0800

    update test

--- modules/accounts/methods_test.go
+++ modules/accounts/methods_test.go
@@ -2,6 +2,7 @@ package accounts_test
 
 import (
 	"context"
+	"database/sql"
 	"math/rand"
 	"testing"
 
@@ -9,11 +10,11 @@ import (
 	mockdb "github.com/muazwzxv/go-backend-masterclass/db/mock"
 	db "github.com/muazwzxv/go-backend-masterclass/db/sqlc"
 	"github.com/muazwzxv/go-backend-masterclass/modules/accounts"
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/assert"
 	"go.uber.org/zap"
 )
 
-func TestGetAccountAPI(t *testing.T) {
+func TestGetAccount(t *testing.T) {
 	testAcc := randomAccount()
 
 	// create mock
@@ -21,32 +22,43 @@ func TestGetAccountAPI(t *testing.T) {
 	defer ctrl.Finish()
 	store := mockdb.NewMockIStore(ctrl)
 
-	// mock
-	store.EXPECT().
-		GetAccount(gomock.Any(), gomock.Eq(testAcc.ID)).
-		Times(1).
-		Return(testAcc, nil)
+	// create module
+	module := createModule(store)
+
+  // setup mock
+  store.EXPECT().
+    GetAccount(gomock.Any(), gomock.Any()).
+    DoAndReturn(func(ctx context.Context, ID int64) (db.Account, error) {
+      if ID == testAcc.ID {
+        return testAcc, nil
+      } 
+      return db.Account{}, sql.ErrNoRows
+    }).
+  AnyTimes()
 
 	type testCase struct {
 		Name        string
-		Account     db.Account
+		AccountID   int64
 		ExpectedErr error
 	}
 
 	testCases := []testCase{
 		{
 			Name:        "Account found",
-			Account:     randomAccount(),
+			AccountID:   testAcc.ID,
 			ExpectedErr: nil,
+		},
+		{
+			Name:        "Account Not found",
+			AccountID:   20,
+			ExpectedErr: accounts.NotFound,
 		},
 	}
 
 	for _, test := range testCases {
 		t.Run(test.Name, func(t *testing.T) {
-			module := createModule(store)
-			acc, err := module.FindAccount(context.Background(), testAcc.ID)
-      require.NoError(t, err)
-      require.NotNil(t, acc)
+			_, err := module.FindAccount(context.Background(), test.AccountID)
+			assert.ErrorIs(t, test.ExpectedErr, err)
 		})
 	}
 }
