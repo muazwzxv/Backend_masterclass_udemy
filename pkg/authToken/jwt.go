94dffe6e9e656a6a380dbe6fbb405a20a844f7c7
Muash <muazwazir@gmail.com>
2025-04-04 20:53:09 +0800

    feat: format

--- pkg/authToken/jwt.go
+++ pkg/authToken/jwt.go
@@ -31,37 +31,36 @@ func (t *Jwt) CreateToken(username string, duration time.Duration) (string, erro
 		return "", err
 	}
 
-  // payload must implements `Valid() error` function
+	// payload must implements `Valid() error` function
 	jwtToken := jwt.NewWithClaims(jwt.SigningMethodHS256, payload)
 
-  return jwtToken.SignedString([]byte(t.secretKey))
+	return jwtToken.SignedString([]byte(t.secretKey))
 }
 
 func (t *Jwt) VerifyToken(token string) (*Payload, error) {
-  jwtToken, err := jwt.ParseWithClaims(token, &Payload{}, func(jwtToken *jwt.Token) (interface{}, error) {
-    // t.Method returns an interface, we try to cast to an implementation
-    _, ok := jwtToken.Method.(*jwt.SigningMethodHMAC)
-    if !ok {
-      // We're using HS256 and HMAC, if casting to HMAC signing method fails, it means the token is invalid or tempered with
-      return nil, ErrInvalidToken
-    }
+	jwtToken, err := jwt.ParseWithClaims(token, &Payload{}, func(jwtToken *jwt.Token) (interface{}, error) {
+		// t.Method returns an interface, we try to cast to an implementation
+		_, ok := jwtToken.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			// We're using HS256 and HMAC, if casting to HMAC signing method fails, it means the token is invalid or tempered with
+			return nil, ErrInvalidToken
+		}
 
-    return []byte(t.secretKey), nil
-  })
+		return []byte(t.secretKey), nil
+	})
+	if err != nil {
+		jwtErr, ok := err.(*jwt.ValidationError)
+		if ok && errors.Is(jwtErr.Inner, ErrTokenExpired) {
+			return nil, ErrTokenExpired
+		}
+		return nil, ErrInvalidToken
+	}
 
-  if err != nil {
-    jwtErr, ok := err.(*jwt.ValidationError)
-    if ok && errors.Is(jwtErr.Inner, ErrTokenExpired) {
-      return nil, ErrTokenExpired
-    }
-    return nil, ErrInvalidToken
-  }
+	payload, ok := jwtToken.Claims.(*Payload)
 
-  payload, ok := jwtToken.Claims.(*Payload)
+	if !ok {
+		return nil, ErrInvalidToken
+	}
 
-  if !ok {
-    return nil, ErrInvalidToken
-  }
-
-  return payload, nil
+	return payload, nil
 }
